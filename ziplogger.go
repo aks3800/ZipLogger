12cd1ce9540d5b843fc5b0579dfc6e079ee2a828
Akshat Sharma <akshat.sharma@myntra.com>
2019-10-14 17:15:56 +0530

    - zip files and delete

--- ziplogger.go
+++ ziplogger.go
@@ -1,6 +1,7 @@
 package ziplogger
 
 import (
+	"archive/zip"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -8,6 +9,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -18,17 +20,18 @@ type LoggerDetails struct {
 }
 
 type cronJobTicker struct {
-	timer *time.Timer
+	timer  *time.Timer
+	ticked int
 }
 
 //IntervalPeriod Time period after which CRON Job will be scheduled
 const IntervalPeriod time.Duration = 24 * time.Hour
 
 //HourToTick Hour at which CRON Job will run
-const HourToTick int = 16
+const HourToTick int = 17
 
 //MinuteToTick Minute at which CRON Job will run
-const MinuteToTick int = 55
+const MinuteToTick int = 9
 
 //SecondToTick Second at which CRON Job will run
 const SecondToTick int = 0
@@ -40,12 +43,18 @@ func (logger LoggerDetails) Init() {
 
 // setUpCRON Function which will schedule CRON JOB
 func setUpCRON(logger LoggerDetails) {
-	jobTicker := &cronJobTicker{}
+	jobTicker := &cronJobTicker{
+		ticked: 0,
+	}
 	jobTicker.updateTimer()
 	for {
 		<-jobTicker.timer.C
 		fmt.Println(time.Now(), "- just ticked")
+		jobTicker.ticked = jobTicker.ticked + 1
 		jobTicker.cronFunctionality(logger.LogFilePath)
+		if jobTicker.ticked == logger.ZipFrequency {
+			zipAndDelete(logger.LogFilePath)
+		}
 		jobTicker.updateTimer()
 	}
 }
@@ -92,5 +101,101 @@ func (t *cronJobTicker) cronFunctionality(logFileName string) {
 	err = ioutil.WriteFile(logFileName, []byte(""), 0666)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func zipAndDelete(logFileName string) {
+	var files []string
+	err := filepath.Walk(filepath.Dir(logFileName), func(path string, info os.FileInfo, err error) error {
+		if path == logFileName {
+			return nil
+		} else if filepath.Ext(path) == ".log" {
+			files = append(files, path)
+		} else if filepath.Ext(path) == "" {
+			if strings.Contains(path, logFileName) {
+				files = append(files, path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	output := "done.zip"
+
+	if err := zipFiles(output, files); err != nil {
+		panic(err)
+	}
+	fmt.Println("Zipped File:", output)
+	for _, file := range files {
+		if file != logFileName {
+			deleteFile(file)
+		}
+	}
+}
+
+// ZipFiles compresses one or many files into a single zip archive file.
+// Param 1: filename is the output zip file's name.
+// Param 2: files is a list of files to add to the zip.
+func zipFiles(filename string, files []string) error {
+
+	newZipFile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer newZipFile.Close()
+
+	zipWriter := zip.NewWriter(newZipFile)
+	defer zipWriter.Close()
+
+	// Add files to zip
+	for _, file := range files {
+		if err = addFileToZip(zipWriter, file); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//addFileToZip Function to add files to zip.
+func addFileToZip(zipWriter *zip.Writer, filename string) error {
+
+	fileToZip, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer fileToZip.Close()
+
+	// Get the file information
+	info, err := fileToZip.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+
+	// Using FileInfoHeader() above only uses the basename of the file. If we want
+	// to preserve the folder structure we can overwrite this with the full path.
+	header.Name = filename
+
+	// Change to deflate to gain better compression
+	// see http://golang.org/pkg/archive/zip/#pkg-constants
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(writer, fileToZip)
+	return err
+}
+
+func deleteFile(path string) {
+	err := os.Remove(path)
+	if err != nil {
+		panic(err)
 	}
 }
